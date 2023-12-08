62d3961322a19269a7bb0b70d4630bfefa14f4ed
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-12-08 18:59:35 -0300

    preparing quiz logic

--- quiz/src/pages/categoriesPages/category/categoryOptions/program/Program.js
+++ quiz/src/pages/categoriesPages/category/categoryOptions/program/Program.js
@@ -1,40 +1,147 @@
-import {useReducer} from 'react';
+import {useReducer, useState} from 'react';
 
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case 'CORRECT' :
-            return {correct: state.correct + 1};
+const initialState = {
+  correct: 0,
+  incorrect: 0,
+  score: 0
+}
 
-            default :
-            return state
-        }
-    };
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'CORRECT' :
+        return {
+          correct: state.correct + 1,
+          incorrect: state.incorrect,
+          score: state.score + 10
+        };
+
+        case 'INCORRECT' :
+        return {
+          correct: state.correct,
+          incorrect: state.incorrect + 1,
+          score: state.score
+        };
+
+        default :
+        return state
+    }
+  };
 
 const Program = () => {
    
 
-    const [state, dispatch] = useReducer(reducer, {correct: 0})
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     const handleAnswer = (answer, event) => {
         event.preventDefault();
 
+
         if (answer === 'CORRECT') {
             dispatch({type: 'CORRECT'})
-        } 
+        } else {
+          dispatch({type: "INCORRECT"})
+        }
     }
     
   return (
     <div>
       <h2>Programação</h2>
-      <span><strong>Score: </strong>{state.correct}</span>
+      <p><strong>Score: </strong>{state.score}</p>
+      <p><strong>Acerto: {state.correct} - Erro: {state.incorrect}</strong></p>
 
-      <form>
-        <label>Qual é a linguagem de programação mais utilizada para desenvolvimento web?</label>
-       <button onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
-       <button onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
-       <button onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
-       <button onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
-      </form>
+        <ul>
+          <li className='li-question-answer'>
+            <span className='question'>1 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>2 - Qual desses não é um tipo de variável em JavaScript?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>let</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>var</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>int</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>const</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>3 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>4 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>5 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>6 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>7 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>8 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>9 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+
+          <li className='li-question-answer'>
+            <span className='question'>10 - Qual é a linguagem de programação mais utilizada para desenvolvimento web?</span>
+        
+            <button className='answer-option' onClick={(e) => handleAnswer('CORRECT', e)}>Javascript</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>C++</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Phyton</button>
+            <button className='answer-option' onClick={(e) => handleAnswer('INCORRECT', e)}>Java</button>
+          </li>
+        </ul>
+        
+
     </div>
   );
 }
