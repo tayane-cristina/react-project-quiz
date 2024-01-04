2180ec24780d2fbd8eecd725c0c68d89438733ae
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-01-04 17:56:58 -0300

    quiz validation complete

--- quiz/src/pages/categoriesPages/category/categoryOptions/program/Program.js
+++ quiz/src/pages/categoriesPages/category/categoryOptions/program/Program.js
@@ -12,40 +12,56 @@ const Program = () => {
 
   const programQuestions = Data.filter(question => question.category === "program")
 
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
+  const buttons = document.querySelectorAll('.answer-option-btn')
 
+  const btnOptionsInitialState = () => {
+    const btnOne = buttons[0]
+    const btnTwo = buttons[1]
+    const btnThree = buttons[2]
+    const btnFour = buttons[3]
+    
+    btnOne.style.backgroundColor = 'blue'
+    btnTwo.style.backgroundColor = 'blue'
+    btnThree.style.backgroundColor = 'blue'
+    btnFour.style.backgroundColor = 'blue'
+  }
+
+  const handleAnswerOptionClick = (e, isCorrect) => {
+
+    const optionTarget = e.target
+
+    buttons.disabled = true
+    if (isCorrect) {
+      optionTarget.style.backgroundColor = "green"
+      setScore(score + 1);
+    } else {
+      optionTarget.style.backgroundColor = "red"
+    }
+    setTimeout(callNextQuestion, 1000)
+  };
+
+  const callNextQuestion = () => {
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < programQuestions.length) {
+      btnOptionsInitialState()
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
     }
   };
-
   
   useEffect(() => {
     const updateScore = score
     if (updateScore <= 3) {
-      setResultMessage("Quando o assunto é esse, você e ele são quase como água e azeite - não se misturam muito, mas é sempre interessante ver a dinâmica! continue a estudar!")
-      
+      setResultMessage("Quando o assunto é esse, você e ele são quase como água e azeite - não se misturam muito, mas é sempre interessante ver a dinâmica! continue a estudar!")      
     } else if (updateScore > 3 && updateScore <= 5) {
-      setResultMessage('Você até está dando os primeiros passos nesse assunto, e com um pouco mais de exploração, tenho certeza de que você se tornará um mestre! Continue a estudar!')
-      
+      setResultMessage('Você até está dando os primeiros passos nesse assunto, e com um pouco mais de exploração, tenho certeza de que você se tornará um mestre! Continue a estudar!')      
     } else if (updateScore > 5 && updateScore <=8) {
       setResultMessage('Você está muito bem nesse assunto! Com mais um pouquinho de dedicação, você vai conquistar a excelência nesse tema. Continue assim, você está no caminho certo para se tornar um expert!') 
-      
     } else {
-      setResultMessage("Você é simplesmente incrível nesse assunto, um verdadeiro 10 de 10! Seu conhecimento é impressionante e inspirador. Continue assim, pois você é uma verdadeira referência nesse tema. Parabéns pelo domínio completo!") 
-      
+      setResultMessage("Você é simplesmente incrível nesse assunto, um verdadeiro 10 de 10! Seu conhecimento é impressionante e inspirador. Continue assim, pois você é uma verdadeira referência nesse tema. Parabéns pelo domínio completo!")   
     }
   }, [score])
-
-  
-  console.log(programQuestions)
-  
 
   return (
     <div className='quiz'>
@@ -66,7 +82,11 @@ const Program = () => {
           </div>
           <div className='answer-section'>
             {programQuestions[currentQuestion].answerOptions.map((answerOption, index) => (
-              <button className='answer-option-btn program' key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+              <button 
+              className='answer-option-btn program' 
+              key={index} 
+              onClick={(e) => handleAnswerOptionClick(e, answerOption.isCorrect)}
+              >
               {answerOption.answerText}
             </button>
             ))}
